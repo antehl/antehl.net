c726809773329492d6cdbdab6f656448a2ce8a89
antehl <antehl.noreply@gmail.com>
2025-02-18 21:17:15 +0100

    disable prerender by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import node from "@astrojs/node";
-
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-    },
-
-  adapter: node({
-    mode: "standalone",
-  }),
-
-  integrations: [alpinejs()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	adapter: node({
+		mode: "standalone",
+	}),
+	integrations: [alpinejs()],
+	output: "server",
 });
